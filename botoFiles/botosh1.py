30e7d2e0f99f47ae13ab36c96c7f649060ec6b1c
xmxyt900 <xmxyt900@gmail.com>
2016-05-02 10:21:05 +1000

    V3

--- botoFiles/botosh1.py
+++ botoFiles/botosh1.py
@@ -1,4 +1,5 @@
 # You need source ec2rc.sh file before running this script
+# This script is used to manage the instances in Nectar Research Cloud
 #!/usr/bin/python
 
 import os
@@ -7,9 +8,15 @@ import boto
 from boto.ec2.connection import EC2Connection
 from boto.ec2.regioninfo import *
 
+
+#The parameters become environment value after source the file downloaded
+#from Nectar Research Cloud 
 EC2_ACCESS_KEY = (os.environ["EC2_ACCESS_KEY"])
 EC2_SECRET_KEY = (os.environ["EC2_SECRET_KEY"])
+
+#Define the cloud region information
 region = RegionInfo(name="melbourne-np", endpoint="nova.rc.nectar.org.au")
+#Connect to the Nectar Cloud with key information and region information
 connection = boto.connect_ec2(aws_access_key_id=EC2_ACCESS_KEY,
                     aws_secret_access_key=EC2_SECRET_KEY,
                     is_secure=True,
@@ -19,19 +26,25 @@ connection = boto.connect_ec2(aws_access_key_id=EC2_ACCESS_KEY,
                     path="/services/Cloud")
 
 reservations = connection.get_all_instances()
-for instance in reservations:
-    print 'id: ', instance.id,
+#for instance in reservations:
+#    print 'id: ', instance.id,
 #images = connection.get_all_images()
 #print images
 
-#Create an instance
-connection.run_instances("ami-000022c5", key_name='Nectar_Key', instance_type='m1.small', security_groups=['NectarTwitterGroup'], placement="melbourne-np")
+#Create 4 instances
+for x in range(0, 4):
+    reservation = connection.run_instances("ami-000022c5", key_name='Nectar_Key', instance_type='m1.small', security_groups=['NectarTwitterGroup'], placement="melbourne-np")
 
-reservations2 = connection.get_all_instances()
-instance = reservations2[0]
-#instance.add_tag("Name","NectarPersonal1")
+#instance = reservation.instances[0]
+#connection.create_tags([instance.id], {"Name": "1"})
 
 
-#Attach the volume to instance
-#vol_req = connection.create_volume(50, "melbourne-np")
-#connection.attch_volume(vol_req.id, instance.id, "/dev/vdc")
+#Create volumes for instances, 3 volumes are 50GB, 1 volume is 100GB
+for x in range(0, 3):
+    vol_req[x] = connection.create_volume(50, "melbourne-np")
+
+vol_req[4] = connection.create_volume(100, "melbourne-np")
+
+#Attach the volumes to instances
+for x in range(0, 4):
+    connection.attch_volume(vol_req[x], reservation.instance[x], "/dev/vdc")
